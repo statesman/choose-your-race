8285fd7a784fa8de33c9b4bd3ced228f4c79f581
Christian McDonald <cmcdonald@statesman.com>
2014-12-21 12:00:54 -0600

    adding images

--- pages/races.php
+++ pages/races.php
@@ -88,27 +88,27 @@
         </div>
         <div class="col-sm-4 col-lg-4">
           <h4>Choose your Race Adventure Start</h4>
-          <div><a href="../"><img class=" img-responsive" src="http://lorempixel.com/400/100/fashion/"></a></div>
+          <div><a href="../"><img class=" img-responsive" src="../assets/choose-your-race/list-start.jpg"></a></div>
           <h4>Running Groups</h4>
-          <div><a href="?list=running-groups"><img class=" img-responsive" src="http://lorempixel.com/400/100/fashion/"></a></div>
+          <div><a href="?list=running-groups"><img class=" img-responsive" src="../assets/choose-your-race/list-running-groups.jpg"></a></div>
           <h4>Fun runs</h4>
-          <div><a href="?list=fun-runs"><img class=" img-responsive" src="http://lorempixel.com/400/100/fashion/"></a></div>
+          <div><a href="?list=fun-runs"><img class=" img-responsive" src="../assets/choose-your-race/list-fun-runs.jpg"></a></div>
           <h4>Novelty runs</h4>
-          <div><a href="?list=novelty-runs"><img class=" img-responsive" src="http://lorempixel.com/400/100/fashion/"></a></div>
+          <div><a href="?list=novelty-runs"><img class=" img-responsive" src="../assets/choose-your-race/list-novelty-runs.jpg"></a></div>
           <h4>Costume runs</h4>
-          <div><a href="?list=costume-runs"><img class=" img-responsive" src="http://lorempixel.com/400/100/fashion/"></a></div>
+          <div><a href="?list=costume-runs"><img class=" img-responsive" src="../assets/choose-your-race/list-costume-runs.jpg"></a></div>
           <h4>Relays</h4>
-          <div><a href="?list=relays"><img class=" img-responsive" src="http://lorempixel.com/400/100/fashion/"></a></div>
+          <div><a href="?list=relays"><img class=" img-responsive" src="../assets/choose-your-race/list-relays.jpg"></a></div>
           <h4>Half-marathons and middle distance</h4>
-          <div><a href="?list=middle-distance"><img class=" img-responsive" src="http://lorempixel.com/400/100/fashion/"></a></div>
+          <div><a href="?list=middle-distance"><img class=" img-responsive" src="../assets/choose-your-race/list-middle-distance.jpg"></a></div>
           <h4>Marathons</h4>
-          <div><a href="?list=marathons"><img class=" img-responsive" src="http://lorempixel.com/400/100/fashion/"></a></div>
+          <div><a href="?list=marathons"><img class=" img-responsive" src="../assets/choose-your-race/list-marathons.jpg"></a></div>
           <h4>Austin Distance Challenge</h4>
-          <div><a href="?list=distance-challenge"><img class=" img-responsive" src="http://lorempixel.com/400/100/fashion/"></a></div>
+          <div><a href="?list=distance-challenge"><img class=" img-responsive" src="../assets/choose-your-race/list-distance-challenge.jpg"></a></div>
           <h4>Shorter trail runs</h4>
-          <div><a href="?list=trail-runs"><img class=" img-responsive" src="http://lorempixel.com/400/100/fashion/"></a></div>
+          <div><a href="?list=trail-runs"><img class=" img-responsive" src="../assets/choose-your-race/list-trail-runs.jpg"></a></div>
           <h4>Ultra trail runs</h4>
-          <div><a href="?list=ultra-trail-runs"><img class=" img-responsive" src="http://lorempixel.com/400/100/fashion/"></a></div>
+          <div><a href="?list=ultra-trail-runs"><img class=" img-responsive" src="../assets/choose-your-race/list-ultras.jpg"></a></div>
 
         </div>
       </div>
