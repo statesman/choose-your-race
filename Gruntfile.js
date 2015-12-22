86074764ad49ca27d6478b65f7e5b032a08a959b
critmcdonald <christian@cmcdonald.com>
2015-12-22 14:25:31 -0600

    adding grunt ftpush and slack hook

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,5 @@
+var fs = require("fs");
+
 module.exports = function(grunt) {
   'use strict';
 
@@ -73,7 +75,52 @@ module.exports = function(grunt) {
         files: ['src/css/**.less'],
         tasks: ['clean:css', 'less']
       }
+    },
+        // stage path needs to be set
+    ftpush: {
+      stage: {
+        auth: {
+          host: 'host.coxmediagroup.com',
+          port: 21,
+          authKey: 'cmg'
+        },
+        src: 'public',
+        dest: '/stage_aas/projects/features/choose-your-race/',
+        exclusions: ['dist/tmp','Thumbs.db','.DS_Store'],
+        simple: false,
+        useList: false
+      },
+      // prod path will need to change
+      prod: {
+        auth: {
+          host: 'host.coxmediagroup.com',
+          port: 21,
+          authKey: 'cmg'
+        },
+        src: 'public',
+        dest: '/stage_aas/projects/features/choose-your-race/',
+        exclusions: ['dist/tmp','Thumbs.db','.DS_Store'],
+        simple: false,
+        useList: false
+      }
+    },
+
+    // be sure to set publishing paths
+    slack: {
+        options: {
+          endpoint: fs.readFileSync('.slack', {encoding: 'utf8'}),
+          channel: '#bakery',
+          username: 'gruntbot',
+          icon_url: 'http://vermilion1.github.io/presentations/grunt/images/grunt-logo.png'
+        },
+        stage: {
+          text: 'Project published to stage: http://stage.host.coxmediagroup.com/aas/projects/features/choose-your-race/ {{message}}'
+        },
+        prod: {
+          text: 'Project published to prod: http://projects.statesman.com/features/choose-your-race/ {{message}}'
+        }
     }
+
 
   });
 
@@ -84,7 +131,13 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-ftpush');
+  grunt.loadNpmTasks('grunt-slack-hook');
+
 
   grunt.registerTask('default', ['copy','jshint', 'clean', 'less', 'uglify']);
+  grunt.registerTask('stage', ['default','ftpush:stage','slack:stage']);
+  grunt.registerTask('prod', ['default','ftpush:prod','slack:prod']);
+
 
 };
